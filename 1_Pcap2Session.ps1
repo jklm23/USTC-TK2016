80ed159f113c30d77d415f26aa3fafa06b534324
jklm23 <30951259+jklm23@users.noreply.github.com>
2023-03-31 16:53:21 +0800

    Update 1_Pcap2Session.ps1

--- 1_Pcap2Session.ps1
+++ 1_Pcap2Session.ps1
@@ -5,15 +5,15 @@
 # can obtain one at http://mozilla.org/MPL/2.0/.
 # ==============================================================================
 
-foreach($f in gci 1_Pcap *.pcap)
+foreach($f in Get-ChildItem 1_Pcap *.pcap)
 {
     0_Tool\SplitCap_2-1\SplitCap -p 50000 -b 50000 -r $f.FullName -o 2_Session\AllLayers\$($f.BaseName)-ALL
     # 0_Tool\SplitCap_2-1\SplitCap -p 50000 -b 50000 -r $f.FullName -s flow -o 2_Session\AllLayers\$($f.BaseName)-ALL
-    gci 2_Session\AllLayers\$($f.BaseName)-ALL | ?{$_.Length -eq 0} | del
+    Get-ChildItem 2_Session\AllLayers\$($f.BaseName)-ALL | Where-Object{$_.Length -eq 0} | Remove-Item
 
     0_Tool\SplitCap_2-1\SplitCap -p 50000 -b 50000 -r $f.FullName -o 2_Session\L7\$($f.BaseName)-L7 -y L7
     # 0_Tool\SplitCap_2-1\SplitCap -p 50000 -b 50000 -r $f.FullName -s flow -o 2_Session\L7\$($f.BaseName)-L7 -y L7
-    gci 2_Session\L7\$($f.BaseName)-L7 | ?{$_.Length -eq 0} | del
+    Get-ChildItem 2_Session\L7\$($f.BaseName)-L7 | Where-Object{$_.Length -eq 0} | Remove-Item
 }
 
 0_Tool\finddupe -del 2_Session\AllLayers
